cd2ca2b89aea8daeb98df36d61c05dfdc766a4a2
Jeff Roth <jjrothfilho@gmail.com>
2022-01-03 16:56:33 -0500

    rm ;; sh

--- ci/install-hub.sh
+++ ci/install-hub.sh
@@ -4,7 +4,6 @@ set -ex
 curl -LsSf https://github.com/github/hub/releases/download/v2.12.8/hub-linux-amd64-2.12.8.tgz -o hub.tgz
 mkdir hub
 tar -xzvf hub.tgz --strip=1 -C hub
-;;
 esac
 
 echo "$PWD/hub/bin" >> $GITHUB_PATH
